d9b94997acfbda6bcc5a9cad4fd91ce98604ba21
Mauricio <mauricio.farias@palmsoft.com.br>
2019-09-03 17:09:11 -0300

    Adicionado renderizando elementos, projeto contem um relogio

---
+++ renderizando_elementos/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, World</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    );
+}
+
+setInterval(tick, 1000);
+
+// ============================================
+
